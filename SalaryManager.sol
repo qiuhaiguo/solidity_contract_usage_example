5e1ce8b6c38bc2ad755faf450dc93258073b0a82
Qiuhai <qiuhai.guo@gmail.com>
2021-04-10 18:44:41 +0200

    add new features

--- SalaryManager.sol
+++ SalaryManager.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 
-contract SalaryManager {
+contract salaryDistribution {
 
     enum Property { notExitAccount,controlAccount, normalAccount} 
     enum Status   { disable, enable}
@@ -17,25 +17,25 @@ contract SalaryManager {
     
     
     
-    mapping(address => Account) public account;
+    mapping(address => Account) public employeeAccount;
     
     constructor () {
-        account[msg.sender].accountProperty = Property.controlAccount;
-        account[msg.sender].accoutStatus = Status.enable;
-        account[msg.sender].withdrawLimit = 2**100;
+        employeeAccount[msg.sender].accountProperty = Property.controlAccount;
+        employeeAccount[msg.sender].accoutStatus = Status.enable;
+        employeeAccount[msg.sender].withdrawLimit = 2**100;
     }
     
     modifier isControlAccount (){
         
-        require(account[msg.sender].accountProperty == Property.controlAccount, "You are not the control account!");
+        require(employeeAccount[msg.sender].accountProperty == Property.controlAccount, "You are not the control account!");
         _;
     }
     
     modifier isWithdrawable (address payable _account,uint _amount) {
         
-        require(account[_account].accountProperty != Property.notExitAccount, "Error, you are not permit to withdraw!");
-        require(account[_account].accoutStatus == Status.enable, "Error, you are not permit to withdraw!");
-        require(_amount<= account[_account].withdrawLimit, "Error, exceed the withdraw limit!");
+        require(employeeAccount[_account].accountProperty != Property.notExitAccount, "Error, you are not permit to withdraw!");
+        require(employeeAccount[_account].accoutStatus == Status.enable, "Error, your account is disabled!");
+        require(_amount<= employeeAccount[_account].withdrawLimit, "Error, exceed the withdraw limit!");
         require(totalBalance >= _amount, "Balance not enough!");
         _;
     }
@@ -46,38 +46,56 @@ contract SalaryManager {
     
     function addAccount(address _account, uint _withdrawLimit) public isControlAccount returns(bool) {
         
-        account[_account].accountProperty = Property.normalAccount;
-        account[_account].accoutStatus = Status.enable;
-        account[_account].withdrawLimit= _withdrawLimit;
+        employeeAccount[_account].accountProperty = Property.normalAccount;
+        employeeAccount[_account].accoutStatus = Status.enable;
+        employeeAccount[_account].withdrawLimit= _withdrawLimit;
+        
+        return true;
+    }
+    
+    function deleteAccount(address _account) public isControlAccount  returns(bool){
+        
+        delete(employeeAccount[_account]);
         
         return true;
     }
     
     function disableAccountWithdraw(address _account) public isControlAccount returns(bool) {
         
-        account[_account].accoutStatus = Status.disable;
+        employeeAccount[_account].accoutStatus = Status.disable;
         
         return true;
     }
     
     function enableAccountWithdraw(address _account) public isControlAccount returns(bool) {
         
-        account[_account].accoutStatus = Status.enable;
+        employeeAccount[_account].accoutStatus = Status.enable;
         
         return true;
     }
     
+    function acountBalanceCheck(address _account) public view returns(uint) {
+        
+        return employeeAccount[_account].withdrawLimit;
+    }
+    
+    function accountStatusCheck(address _account) public view returns(Status) {
+        
+        return employeeAccount[_account].accoutStatus;
+    }
+    
+    
     function withdrawTo(address payable _account, uint _amount) public isWithdrawable(_account,_amount) returns(bool) {
         
-        account[_account].withdrawLimit -= _amount;
+        employeeAccount[_account].withdrawLimit -= _amount;
         totalBalance -= _amount;
         _account.transfer(_amount);
-
         
         return true;
     }
     
     function destroyContract() public  isControlAccount returns(bool){
+        
         selfdestruct(payable(msg.sender));
         return true;
     }
